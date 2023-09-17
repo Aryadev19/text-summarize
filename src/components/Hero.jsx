536be4feb019c3cb70540db80d5159b7cd5c905b
Piyush Aryaprakash <piyusharyaprakash12345@gmail.com>
2023-09-17 16:20:30 +0530

    fixed the fronted a bit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,11 @@ import { logo } from "../assets";
 const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center w-full mb-10 pt-3">
-      </nav>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3"></nav>
 
       <h1 className="head_text">
         Summarize Articles with <br className="max-md:hidden" />
-        <span className="orange_gradient ">OpenAI GPT-4</span>
+        <span className="orange_gradient ">Rapid Ai</span>
       </h1>
       <h2 className="desc">
         Simplify your reading with Summize, an open-source article summarizer
